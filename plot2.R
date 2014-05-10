edf0c703185affdb11a42bea679392a9d6e9eea7
Kevin <kevin-y@users.noreply.github.com>
2014-05-10 22:00:03 +0800

    Delete plot2.R

--- plot2.R
+++
@@ -1,7 +0,0 @@
-if(!exists('subHpc')) {
-	source('load_data.R')
-}
-png("plot2.png")
-plot(subHpc$Global_active_power, type = "l", ylab = "Global Active Power(kilowatts)", xaxt = "n", xlab = "")
-axis(1, at = c(0, 1500, 2880), labels = c("Thu", "Fri", "Sat"))
-dev.off()
