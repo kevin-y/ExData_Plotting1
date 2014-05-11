906f9cb1cb8f7cc4d3c0bbfcd94982d211ce0a5c
Kevin Young <kevin-y@users.noreply.github.com>
2014-05-11 23:01:29 +0800

    Update Course Project codes

--- CourseProject1/plot2.R
+++ CourseProject1/plot2.R
@@ -2,6 +2,8 @@ if(!exists('subHpc')) {
 	source('load_data.R')
 }
 png("plot2.png")
-plot(subHpc$Global_active_power, type = "l", ylab = "Global Active Power(kilowatts)", xaxt = "n", xlab = "")
-axis(1, at = c(0, 1500, 2880), labels = c("Thu", "Fri", "Sat"))
+time <- strptime(paste(subHpc$Date, subHpc$Time), "%d/%m/%Y %H:%M:%S")
+plot(time, subHpc$Global_active_power, type = "l", ylab = "Global Active Power(kilowatts)", xaxt = "n", xlab = "")
+dates <- as.POSIXct(round(range(time), "days"))
+axis.POSIXct(1, at = seq(dates[1], dates[2], by = "days"))
 dev.off()
